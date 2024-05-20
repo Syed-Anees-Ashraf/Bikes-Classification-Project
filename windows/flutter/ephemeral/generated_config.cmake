8d5048bfec49f664dd2dea87206d2d3c4c9c02aa
Syed-Anees-Ashraf <syedaneesashraf426@gmail.com>
2024-05-20 23:00:02 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\image\\bike" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\flutter\\flutter"
+  "PROJECT_DIR=E:\\image\\bike"
+  "FLUTTER_ROOT=E:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\image\\bike\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\image\\bike"
+  "FLUTTER_TARGET=E:\\image\\bike\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\image\\bike\\.dart_tool\\package_config.json"
+)
